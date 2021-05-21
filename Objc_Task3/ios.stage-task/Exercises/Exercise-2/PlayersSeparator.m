e75380b462e955667cdf90d9b91f0d67e5fb6aca
nadyaios <nzenckova.ios@gmail.com>
2021-05-21 19:11:49 +0300

    feat: exercise 2-3 implemented

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,23 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger numberOfTeams = 0;
+    
+    for (NSInteger i = 0; i < ratingArray.count; i++) {
+        for (NSInteger j = i + 1; j < ratingArray.count; j++) {
+            for (NSInteger k = j + 1; k < ratingArray.count; k++) {
+                NSUInteger iPlayerScore = [ratingArray[i] integerValue];
+                NSUInteger jPlayerScore = [ratingArray[j] integerValue];
+                NSUInteger kPlayerScore = [ratingArray[k] integerValue];
+                BOOL areTeamsIncluded = ((iPlayerScore < jPlayerScore && jPlayerScore < kPlayerScore) || (iPlayerScore > jPlayerScore && jPlayerScore > kPlayerScore)) && (0 <= i < j < k < ratingArray.count);
+                if (areTeamsIncluded) {
+                    numberOfTeams++;
+                }
+            }
+        }
+    }
+    
+    return numberOfTeams;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,38 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSUInteger string1Length = string1.length;
+    NSUInteger string2Length = string2.length;
+    NSString *shortestString = string1Length < string2Length ? string1 : string2;
+    NSString *longestString = string1Length > string2Length ? string1 : string2;
+    
+    unichar bufferShort[shortestString.length + 1];
+    unichar bufferLong[longestString.length + 1];
+    [string1 getCharacters:bufferShort range:NSMakeRange(0, string1Length)];
+    [string2 getCharacters:bufferLong range:NSMakeRange(0, string2Length)];
+    
+    NSMutableString *processingResult = [NSMutableString string];
+    NSInteger i = 0, j = 0;
+    
+    while (i < shortestString.length) {
+        if (bufferShort[i] <= bufferLong[j]) {
+            [processingResult appendFormat:@"%c", bufferShort[i]];
+            i++;
+        } else {
+            [processingResult appendFormat:@"%c", bufferLong[j]];
+            j++;
+        }
+        
+        
+    }
+    
+    if (processingResult.length < (string1Length + string2Length)) {
+        [processingResult appendString:[longestString substringFromIndex:j]];
+    }
+    
+    NSString *result = [NSString stringWithString:processingResult];
+        
+    return result;
 }
 
 @end
