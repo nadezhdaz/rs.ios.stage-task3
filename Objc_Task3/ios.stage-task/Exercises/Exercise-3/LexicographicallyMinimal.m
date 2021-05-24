6e155534b2f2af1298e9dd9819e218ffa98cf6f2
nadyaios <nzenckova.ios@gmail.com>
2021-05-24 22:55:46 +0300

    fix: exercise 3 fixed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,14 +9,14 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    NSString *shortestString = string1.length < string2.length ? string1 : string2;
+    NSString *shortestString = string1.length <= string2.length ? string1 : string2;
     NSString *longestString = string1.length > string2.length ? string1 : string2;
     
     unichar bufferShort[shortestString.length + 1];
     unichar bufferLong[longestString.length + 1];
     
-    [string1 getCharacters:bufferShort range:NSMakeRange(0, string1.length)];
-    [string2 getCharacters:bufferLong range:NSMakeRange(0, string2.length)];
+    [shortestString getCharacters:bufferShort range:NSMakeRange(0, shortestString.length)];
+    [longestString getCharacters:bufferLong range:NSMakeRange(0, longestString.length)];
     
     NSMutableString *result = [NSMutableString string];
     NSInteger i = 0, j = 0;
